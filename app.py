e7e4ef934aab27fd7ae1424e3d9039965bf79242
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2025-11-17 15:10:22 +0000

    more examples

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from xword import matches
 
 app = Flask(__name__)
 
 @app.get("/hello")
 def hello():
-    return "Hello from the smallest Flask webapp there is!"
+    return "Hello from /hello!"
+
+@app.get("/bye")
+def byebye():
+    return "I'm so sorry to see you go (sniff)..."
+
+@app.get("/whoops")
+def omg():
+    first = 10
+    second = 1
+    result = first / second
+    return render_template("whoops.html", result=result)
+
+@app.get("/words")
+def words():
+    return matches("f___")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
