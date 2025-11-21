30e2e2d21367db3a2aac94f0092b6e3f1809301d
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2025-11-21 15:57:34 +0000

    basic xword

---
+++ app-test.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from xword import matches
+
+app = Flask(__name__)
+
+@app.get("/hello")
+def hello():
+    return render_template(
+        "hello.html",
+        the_title="This is HELLO",
+    )
+
+@app.get("/bye")
+def byebye():
+    return "I'm so sorry to see you go (sniff)..."
+
+@app.get("/whoops")
+def omg():
+    first = 10
+    second = 300
+    return render_template(
+        "whoops.html",
+        result=first/second,
+        the_title="The results are in!",
+    )
+
+@app.get("/words")
+def words():
+    return matches("f___")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
--- app.py
+++ app.py
@@ -1,27 +1,25 @@
-from flask import Flask, render_template
-from xword import matches
+from flask import Flask, render_template, request
+import xword
 
 app = Flask(__name__)
 
-@app.get("/hello")
-def hello():
-    return render_template(
-        "hello.html",
-        the_title="This is HELLO",
-    )
-
-@app.get("/bye")
-def byebye():
-    return "I'm so sorry to see you go (sniff)..."
-
-@app.get("/whoops")
-def omg():
+@app.get("/")
+def opening_page():
     first = 10
     second = 300
     return render_template(
-        "whoops.html",
-        result=first/second,
-        the_title="The results are in!",
+        "pattern.html",
+        the_title="Welcome to Xword on the Web!",
+    )
+
+@app.post("/processpattern")
+def get_the_results():
+    pat = request.form["pattern"]
+    results = xword.find_possible_matches(pat)
+    return render_template(
+        "results.html",
+        the_title="Possible Matches",
+        the_results=results
     )
 
 @app.get("/words")
